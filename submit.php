d8d6b41f2105087307c52a43ed5de008a96e0a12
Jorys Paulin <paulinjorys5@gmail.com>
2018-11-27 18:30:03 +0100

    Clean-up of processing code

--- submit.php
+++ submit.php
@@ -1,30 +1,55 @@
 <?php
 $dataFolder = __DIR__.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR;
-
-//header('Content-type: text/csv');
-//header('Content-disposition: attachment;filename=database.csv');
-
-$str = "";
-$str .= ($_POST['city'] ?? '').','; // $str .= ($_POST['city'] ? $_POST['city'] : '').',';
-$str .= ($_POST['ageRadio'] ?? '').',';
-$str .= ($_POST['healthSectors'] ?? '').',';
-$str .= ($_POST['hasHealthSectorsProblems'] ?? '').',';
-$str .= ($_POST['healthSectorsProblems'] ?? '').',';
-$str .= ($_POST['publicSectors'] ?? '').',';
-$str .= ($_POST['hasPublicSectorsProblems'] ?? '').',';
-$str .= ($_POST['publicSectorsProblems'] ?? '').',';
-$str .= ($_POST['shopsSectors'] ?? '').',';
-$str .= ($_POST['hasShopsSectorsProblems'] ?? '').',';
-$str .= ($_POST['shopsSectorsProblems'] ?? '').',';
-$str .= ($_POST['hobbiesSectors'] ?? '').',';
-$str .= ($_POST['hasHobbiesSectorsProblems'] ?? '').',';
-$str .= ($_POST['hobbiesSectorsProblems'] ?? '').',';
-$str .= ($_POST['publicTransportUseful'] ?? '').',';
-$str .= ($_POST['publicTransportRate'] ?? '').',';
-$str .= ($_POST['otherTransports'] ?? '');
-$str .= "\n";
-
-$contents = file_get_contents($dataFolder.'database.csv');
-$contents .= $str;
-file_put_contents($dataFolder.'database.csv', $contents);
-printf($str);
+$separator = ';';
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>La mobilité sur le territoire - Codecom des Portes de Meuse</title>
+    <meta name="Description" content="Questionnaire visant à recueillir les besoins de la mobilité des habitants du territoire Portes de Meuse">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  </head>
+  <body class="bg-light">
+    <div class="container">
+      <h1 class="text-center my-4">Enregistrement de vos réponses</h1>
+      <?php if(!empty($_POST)) {
+        $str = "";
+        $str .= ($_POST['city'] ?? '').$separator; // $str .= ($_POST['city'] ? $_POST['city'] : '').$separator;
+        $str .= ($_POST['ageRadio'] ?? '').$separator;
+        $str .= ($_POST['healthSectors'] ?? '').$separator;
+        $str .= ($_POST['hasHealthSectorsProblems'] ?? '').$separator;
+        $str .= ($_POST['healthSectorsProblems'] ?? '').$separator;
+        $str .= ($_POST['publicSectors'] ?? '').$separator;
+        $str .= ($_POST['hasPublicSectorsProblems'] ?? '').$separator;
+        $str .= ($_POST['publicSectorsProblems'] ?? '').$separator;
+        $str .= ($_POST['shopsSectors'] ?? '').$separator;
+        $str .= ($_POST['hasShopsSectorsProblems'] ?? '').$separator;
+        $str .= ($_POST['shopsSectorsProblems'] ?? '').$separator;
+        $str .= ($_POST['hobbiesSectors'] ?? '').$separator;
+        $str .= ($_POST['hasHobbiesSectorsProblems'] ?? '').$separator;
+        $str .= ($_POST['hobbiesSectorsProblems'] ?? '').$separator;
+        $str .= ($_POST['publicTransportUseful'] ?? '').$separator;
+        $str .= ($_POST['publicTransportRate'] ?? '').$separator;
+        $str .= ($_POST['otherTransports'] ?? '');
+        $str .= "\n";
+        $contents = file_get_contents($dataFolder.'database.csv');
+        $contents .= $str;
+        if(file_put_contents($dataFolder.'database.csv', $contents)) { ?>
+          <div class="alert alert-success" role="alert">
+            Merci d'avoir participé à ce questionnaire! Vous pouvez à présent fermer cette fenêtre.
+          </div>
+        <?php } else { ?>
+          <div class="alert alert-danger" role="alert">
+            Oups! Une erreur s'est produite! <a href="./" class="alert-link">Recommencer</a>
+          </div>
+        <?php }
+      } else { ?>
+        <div class="alert alert-danger" role="alert">
+          Oups! On dirait que votre questionnaire n'a pas été envoyé! <a href="./" class="alert-link">Recommencer</a>
+        </div>
+      <?php } ?>
+    </div>
+  </body>
+</html>
